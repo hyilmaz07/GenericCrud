3a3f6adb4ad937b5f9eda82424271c2849a15a2e
Hüseyin YILMAZ <hyilmaz07@outlook.com>
2021-05-22 23:20:29 +0300

    log model ve log repo

---
+++ GenericCrud.Core/Repositories/LogRepository.cs
@@ -0,0 +1,70 @@
+﻿using Dapper.Contrib.Extensions;
+using GenericCrud.Entity;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCrud.Core.Repositories
+{
+ public   class LogRepository:Connection.Connection
+    {
+        public int Insert(Logs model)
+        {
+            try
+            {
+                using (var con =GetConnection)
+                {
+                    return int.Parse(con.Insert(model).ToString());
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public static void CreateLog(Exception ex)
+        {
+            try
+            {
+                var st = new StackTrace(ex, true);
+                if (st != null)
+                {
+                    st.GetFrames().Where(k => k.GetFileLineNumber() > 0).ToList().ForEach(k =>
+                    {
+                        new LogRepository().Insert(new Logs()
+                        {
+                            CreatedDate = DateTime.Now,
+                            Message = ex.Message,
+                            Source = ex.Source + " | " + k,
+                            Line = k.GetFileLineNumber()
+                        });
+                    });
+                }
+                else
+                {
+                    new LogRepository().Insert(new Logs()
+                    {
+                        CreatedDate = DateTime.Now,
+                        Message = ex.Message,
+                        Source = ex.Source,
+                        Line = 0
+                    });
+                }
+            }
+            catch (Exception exception)
+            {
+                new LogRepository().Insert(new Logs()
+                {
+                    CreatedDate = DateTime.Now,
+                    Message = exception.Message,
+                    Source = exception.Source + " - CreateLogta Sorun var.",
+                    Line = 0
+                });
+            }
+        }
+    }
+}
---
+++ GenericCrud.Entity/Logs.cs
@@ -0,0 +1,24 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCrud.Entity
+{
+    [Table("Logs")]
+   public class Logs
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Message { get; set; }
+
+        public string Source { get; set; }
+
+        public int? Line { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+    }
+}
