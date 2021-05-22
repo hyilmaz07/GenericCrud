37e30e363566f0bbd02e21356e95e2cf0b9f4a5e
Hüseyin YILMAZ <hyilmaz07@outlook.com>
2021-05-22 23:38:26 +0300

    Audit model ve compate method

---
+++ GenericCrud.Core/Tool/Audit.cs
@@ -0,0 +1,41 @@
+﻿using GenericCrud.Entity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCrud.Core.Tool
+{
+    public static class Audit
+    {
+        static string Compare<T>(object _object1, object _object2)
+        {
+            Type type = typeof(T);
+            PropertyInfo[] properties = type.GetProperties();
+            List<AuditTracing> detailList = new List<AuditTracing>();
+            foreach (PropertyInfo property in properties)
+            {
+                object firstValue = property.GetValue(_object1);
+                object secondValue = property.GetValue(_object2);
+                if (!object.Equals(firstValue, secondValue))
+                {
+                    AuditTracing details = new AuditTracing();
+                    details.Property = property.Name;
+                    details.OldValue = firstValue.ToString();
+                    details.NewValue = secondValue.ToString();
+                    detailList.Add(details);
+                }
+            }
+            return Serialize(detailList);
+        }
+        static string Serialize(this object obj)
+        {
+            var serializerSettings = new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace };
+            var serializedObject = JsonConvert.SerializeObject(obj, serializerSettings);
+            return serializedObject;
+        }
+    }
+}
---
+++ GenericCrud.Entity/AuditTracing.cs
@@ -0,0 +1,27 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCrud.Entity
+{
+    [Table("AuditTracing")]
+    public class AuditTracing : BaseEntity
+    {
+        public string Object { get; set; }
+        public int ReferanceID { get; set; }
+        public string Property { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public AuditType AuditType { get; set; }
+    }
+    public enum AuditType
+    {
+        Insert = 1,
+        Update = 2,
+        Delete = 3,
+        Remove = 4
+    }
+}
